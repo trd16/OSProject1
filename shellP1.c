c4c8ae6b277cb1ec1458a066c53dc7903098c717
trd16 <31514871+trd16@users.noreply.github.com>
2020-01-28 20:48:54 -0500

    Update shellP1.c

--- shellP1.c
+++ shellP1.c
@@ -745,6 +745,7 @@ void pipeImplementation(char* command1, char* command2)
 {
 	printf("Found |\n");
 	/*
+	int status;
 	int fd[2];
 	if(fork() == 0)
 	{
@@ -775,8 +776,8 @@ void pipeImplementation(char* command1, char* command2)
 		//parent shell
 		close(fd[0]);
 		close(fd[1]);
-		waitpid(pid1, NULL, 0);
-		waitpid(pid2, NULL, 0);
+		waitpid(pid1, &status, 0);
+		waitpid(pid2, &status 0);
 	}
 	*/
 }
